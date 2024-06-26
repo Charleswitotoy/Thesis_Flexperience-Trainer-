9407a3e548f53d35e16727541b345f97edc9a457
Charles Cuizon <Charlescuizon2003@gmail.com>
2024-06-26 15:02:05 +0800

    June 26

--- script/script.js
+++ script/script.js
@@ -15,3 +15,12 @@ function cancel_createWorkout(){
     var createWorkout_popup = document.getElementById('create_new_workout_pop_up')
     createWorkout_popup.classList.toggle('active')
 }
+
+function viewWorkout(){
+    var viewWorkout_popup = document.getElementById('view_workout_popup')
+    viewWorkout_popup.classList.toggle('active')
+}
+function cancel_viewWorkout(){
+    var viewWorkout_popup = document.getElementById('view_workout_popup')
+    viewWorkout_popup.classList.toggle('active')
+}
