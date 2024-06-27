f80c09b83677f4b4197cb0a90d8e285d10deb7c7
Charles Cuizon <Charlescuizon2003@gmail.com>
2024-06-27 16:48:51 +0800

    June 27

--- script/script.js
+++ script/script.js
@@ -15,17 +15,192 @@ function cancel_createWorkout(){
     var createWorkout_popup = document.getElementById('create_new_workout_pop_up')
     createWorkout_popup.classList.toggle('active')
 }
-
-function viewWorkout(){
-    var viewWorkout_popup = document.getElementById('view_workout_popup')
-    viewWorkout_popup.classList.toggle('active')
-}
 function cancel_viewWorkout(){
     var viewWorkout_popup = document.getElementById('view_workout_popup')
     viewWorkout_popup.classList.toggle('active')
 }
 
 
-function reuse(){
-    
+function viewWorkout(planType){
+    alert(planType)
+
+    if (planType == 'Bulking Workout Plan'){
+        const form = document.getElementById('view_workout_popup');
+        form.innerHTML = `
+        <div> 
+            <h1>${planType} Details</h1>
+        </div>
+            <table>
+               <tr>
+                    <th>Exercise Name</th>
+                    <th>Repititions</th>
+                    <th>Sets</th>
+                    <th>Target Muscle Group</th>
+                    <th>Week No</th>
+                    <th>Day No</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+            </table>
+        <div>
+            <input class="button" type="button" value="CLOSE" onclick="cancel_viewWorkout()">'
+        </div>
+        `
+    }else if(planType == 'Strength Training Workout Plan'){
+        const form = document.getElementById('view_workout_popup');
+        form.innerHTML = `
+        <div> 
+            <h1>${planType} Details</h1>
+        </div>
+            <table>
+               <tr>
+                    <th>Exercise Name</th>
+                    <th>Repititions</th>
+                    <th>Sets</th>
+                    <th>Target Muscle Group</th>
+                    <th>Week No</th>
+                    <th>Day No</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+            </table>
+        <div>
+            <input class="button" type="button" value="CLOSE" onclick="cancel_viewWorkout()">'
+        </div>
+        `
+    }else if(planType == 'Body Building Workout Plan'){
+        const form = document.getElementById('view_workout_popup');
+        form.innerHTML = `
+        <div> 
+            <h1>${planType} Details</h1>
+        </div>
+            <table>
+               <tr>
+                    <th>Exercise Name</th>
+                    <th>Repititions</th>
+                    <th>Sets</th>
+                    <th>Target Muscle Group</th>
+                    <th>Week No</th>
+                    <th>Day No</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+            </table>
+        <div>
+            <input class="button" type="button" value="CLOSE" onclick="cancel_viewWorkout()">'
+        </div>
+        `
+    }else if(planType == 'Shredding Workout Plan'){
+        const form = document.getElementById('view_workout_popup');
+        form.innerHTML = `
+        <div> 
+            <h1>${planType} Details</h1>
+        </div>
+            <table>
+               <tr>
+                    <th>Exercise Name</th>
+                    <th>Repititions</th>
+                    <th>Sets</th>
+                    <th>Target Muscle Group</th>
+                    <th>Week No</th>
+                    <th>Day No</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+            </table>
+        <div>
+            <input class="button" type="button" value="CLOSE" onclick="cancel_viewWorkout()">'
+        </div>
+        `
+    }
+    var viewWorkout_popup = document.getElementById('view_workout_popup')
+    viewWorkout_popup.classList.toggle('active')
 }
